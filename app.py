ef2bb65b4988940f05f26a666a82f157b870a73b
kajavatana <kajavatana@gmail.com>
2019-02-08 16:31:38 +0530

    123

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
+import os
 from flask import Flask
-from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+def hello():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
